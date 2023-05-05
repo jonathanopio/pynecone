f531664370f4e99eaea94abbdbaa64ea11d09eb3
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2023-05-04 21:49:35 -0400

    Had removed logic briefly for debugging, re-adding it because it was forgotten

--- pynecone/components/typography/text.py
+++ pynecone/components/typography/text.py
@@ -16,6 +16,15 @@ class Text(ChakraComponent):
     @classmethod
     def create(cls, *children, **props):
         
-        
+        # Correct quotes to prevent compile error
+        for key, value in props.items():
+            print(type(value))
+            if type(value) == str:
+                print("this happened")
+                splits = value.split(',')
+                for i, split in enumerate(splits):
+                    if split.startswith('"') and split.endswith('"'):
+                        splits[i] = split.replace('"', "'")
+                        props[key] = ','.join(splits)
                         
         return super().create(*children, **props)
