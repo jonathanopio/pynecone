794021dac3196175067f9707e69bce74dc6bcbad
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2023-05-06 23:57:23 -0400

    Changes to html file moves from incorrect branch to here

--- pynecone/components/layout/html.py
+++ pynecone/components/layout/html.py
@@ -1,8 +1,10 @@
 """A html component."""
 
-from typing import Any
+from typing import Dict
 
 from pynecone.components.layout.box import Box
+from pynecone.components.typography.heading import Heading
+from pynecone.var import Var
 
 
 class Html(Box):
@@ -13,7 +15,7 @@ class Html(Box):
     """
 
     # The HTML to render.
-    dangerouslySetInnerHTML: Any
+    dangerouslySetInnerHTML: Var[Dict]
 
     @classmethod
     def create(cls, *children, **props):
@@ -29,11 +31,52 @@ class Html(Box):
         Raises:
             ValueError: If children are not provided or more than one child is provided.
         """
+        # check for heading
+
         # If children are not provided, throw an error.
         if len(children) != 1:
             raise ValueError("Must provide children to the html component.")
         else:
             props["dangerouslySetInnerHTML"] = {"__html": children[0]}
 
-        # Create the component.
-        return super().create(**props)
+        
+        
+        string = children[0]
+        text, format = cls.Format_Headings(string)
+        if (format != ''):
+            return Heading().create(text, size=format)
+        else:
+            # Create the component.
+            return super().create(**props)
+
+    @classmethod
+    def Format_Headings(cls, string):
+        heading = 0
+        text = ""
+        format = ''
+        for i in range(len(string)-2):
+            if (string[i]=='<' and string[i+1]=='h'):
+                if(string[i+2]).isdigit() and (int(string[i+2])<=6 and int(string[i+2])>0):
+                    heading = int(string[i+2])
+                    break
+        if heading != 0:
+            for i in range(len(string)):
+                if string[i] == '>':
+                    text = ""
+                else: text += string[i]
+                if string[i+1]=="<" and string[i+2]=='/':
+                    break
+            format = ""
+            if heading == 1:
+                format = '3xl'
+            elif heading == 2:
+                format = '2xl'
+            elif heading == 3:
+                format = 'xl'
+            elif heading == 4:
+                format = 'lg'
+            elif heading == 5:
+                format = 'md'
+            elif heading == 6:
+                format = 'sm'
+        return text, format
